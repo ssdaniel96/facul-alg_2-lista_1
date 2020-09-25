659debf50f94644004daa58e09686a59c47609f7
Daniel da Silva Soares <daniel_soares2007@hotmail.com>
2020-09-24 21:30:53 -0300

    Ex1 print in file

--- Ex1/src/main.cpp
+++ Ex1/src/main.cpp
@@ -1,21 +1,34 @@
 #include <iostream>
-
+#include <string.h>
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-	float price = 0.0;
+	float price = 0.0, inflacionado = 0.0;
 	bool cond = true;
 	cout << "Digite um valor: ";
 	cin >> price;
 
-	while(price <= 100 && cond){
-		cout << "Preco inflacionado: " << price * 1.1;
+	while (price <= 100 && cond)
+	{
+		inflacionado = price * 1.1;
+		cout << "Preco inflacionado: " << inflacionado << endl;
 		cond = false;
 	}
 
-	while(price > 100 && cond){
-		cout << "Preco inflacionado: " << price * 1.2;
+	while (price > 100 && cond)
+	{
+		inflacionado = price * 1.2;
+		cout << "Preco inflacionado: " << inflacionado << endl;
 		cond = false;
 	}
+
+	char nome[30];
+	cout << "Digite o nome para salvar: ";
+	strcat(nome, ".txt");
+	cin >> nome;
+	cin.ignore();
+	FILE *arquivo = fopen(nome, "w");
+	fprintf(arquivo, "O preco informado foi: %.2f e o preco inflacionado eh: %.2f", price, inflacionado);
+	return 0;
 }
